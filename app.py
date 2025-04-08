453048647c047a577a41a07fa71a204dc882295d
Arjun32166 <arjunsprasad13@gmail.com>
2025-04-08 15:21:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ a5=st.number_input("Enter Sunshine")
 a6=st.number_input("Enter Winddirection")
 a7=st.number_input("Enter Windspeed")
 
-st.button("predict") 
-      op = mymodel.predict ([[a1,a2,a3,a4,a5,a6,a7]])
-      if op==1:
-         st.write('Barish Hogi!!!')
+if st.button('Predict'):
+    op = mymodel.predict([[a1,a2,a3,a4,a5,a6,a7]])[0]
+    if op == 1:
+        st.write('Barish Hogi!!!')
+    else:
+        st.write('Barish Nahi Hogi!!!')
